8b8333387824098640e69d00e7d235085b4cd43e
Серков Евгений <exzojs@gmail.com>
2022-07-18 23:53:55 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int client = 200;
+        int summa = 2000;
+        int bonus = summa / 100 + summa;
+        int bonusRub = summa / 100;
+        boolean sBonusom = summa >= 1000;
+        int itog = sBonusom ? bonus : summa;
+        int bonusShow = sBonusom ? bonusRub : 0;
+        System.out.println(itog + client + " Итоговый счет");
+        System.out.println(bonusShow + " Бонусные рубли");
+
+
+    }
+}
